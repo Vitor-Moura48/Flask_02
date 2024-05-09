58cdda82ee709c3db1e6e96d1b290805e942c1d7
Vitor Gabriel <gabriel.4815.7@gmail.com>
2024-05-09 19:12:41 -0300

    Adicionando o caminho do database temporário

--- database/database.py
+++ database/database.py
@@ -1,6 +1,4 @@
 from peewee import PostgresqlDatabase
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-db = PostgresqlDatabase(os.getenv('DATABASE_URI', ''))
+        # database temporário, caso não esteja em funcionamento, substitua o caminho por um banco de dados ativo
+db = PostgresqlDatabase('postgresql://postgres.cdstiiwzfbsjpkewvpqt:EFrWj4mXCVzd6F01@aws-0-sa-east-1.pooler.supabase.com:5432/postgres') 
